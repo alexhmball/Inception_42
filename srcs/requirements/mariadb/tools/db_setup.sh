9ecefa06718ac88a7d70a44b950edfbe5b96dfcf
aball <aball@debian.42.fr>
2023-05-27 12:32:57 -0400

    removed files

--- srcs/requirements/mariadb/tools/db_setup.sh
+++ srcs/requirements/mariadb/tools/db_setup.sh
@@ -5,8 +5,8 @@ rc-status
 rc-service mariadb start && rc-update add mariadb default
 /etc/init.d/mariadb restart
 mariadb << EOF
-CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
 GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';
 FLUSH PRIVILEGES;
